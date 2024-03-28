a734990d663b573b3cac0c68276859ddcaa6e692
ojochau <ojochau@instajs.com>
2024-03-28 15:03:17 +0800

    update test

--- test/modules/module1/index.tsx
+++ test/modules/module1/index.tsx
@@ -9,15 +9,29 @@ export default class Module1 extends Module {
         super(parent, options);
     }
 
+    init() {
+        super.init();
+        this.scomQRCode.text = 'Testing';
+        this.scomQRCode.size = 128;
+        this.scomQRCode.level = 'M';
+        this.scomQRCode.qrCodeBackground = { color: '#fff', alpha: 0.9 };
+        this.scomQRCode.qrCodeForeground = { color: '#fe5262', alpha: 0.9 };
+        this.scomQRCode.updateQRCode();
+    }
+
     render() {
         return (
             <i-hstack padding={{ top: '1.25rem', bottom: '1.25rem', left: '1.25rem', right: '1.25rem' }} gap="1rem">
-                <i-panel margin={{ left: 'auto', right: 'auto' }}>
+                <i-hstack gap="1rem">
+                    <i-scom-qr-code id="scomQRCode"></i-scom-qr-code>
                     <i-scom-qr-code
-                        id="scomQRCode"
                         text="lnbc2500u1pvjluezsp5zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zygspp5qqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqqqsyqcyq5rqwzqfqypqdq5xysxxatsyp3k7enxv4jsxqzpu9qrsgquk0rl77nj30yxdy8j9vdx85fkpmdla2087ne0xh8nhedh8w27kyke0lp53ut353s06fv3qfegext0eh0ymjpf39tuven09sam30g4vgpfna3rh"
+                        size={256}
+                        level="L"
+                        qrCodeBackground={{ color: '#007abf', alpha: 0.8 }}
+                        qrCodeForeground={{ color: '#fff' }}
                     ></i-scom-qr-code>
-                </i-panel>
+                </i-hstack>
             </i-hstack>
         )
     }
